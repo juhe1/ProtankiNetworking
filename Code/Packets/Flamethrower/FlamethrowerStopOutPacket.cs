e2f2f51bc0d8258334e13ea22531aa19c0746404
juho <you@example.com>
2026-04-09 21:38:48 +0300

    feat: add type for flamethrowerstopoutpacket client time

--- Code/Packets/Flamethrower/FlamethrowerStopOutPacket.cs
+++ Code/Packets/Flamethrower/FlamethrowerStopOutPacket.cs
@@ -1,12 +1,9 @@
-using ProtankiNetworking.EncodableData;
-
 namespace ProtankiNetworking.Packets.Flamethrower;
 
 public class FlamethrowerStopOutPacket : Packet
 {
-
 	[Encode(0)]
-	public ClientTime { get; set; }
+	public int ClientTime { get; set; }
 
 	public const int ID_CONST = -1300958299;
 	public override int Id => ID_CONST;
