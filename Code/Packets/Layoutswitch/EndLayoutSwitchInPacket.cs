0632f440a8569cf38a230a5903d1a96f18e72572
juho <you@example.com>
2026-05-11 02:27:48 +0300

    feat: EndLayoutSwitchInPacket change nullability

--- Code/Packets/Layoutswitch/EndLayoutSwitchInPacket.cs
+++ Code/Packets/Layoutswitch/EndLayoutSwitchInPacket.cs
@@ -4,12 +4,11 @@ namespace ProtankiNetworking.Packets.Layoutswitch;
 
 public class EndLayoutSwitchInPacket : Packet
 {
-
 	[Encode(0)]
-	public LayoutState? Origin { get; set; }
+	public LayoutState Origin { get; set; }
 
 	[Encode(1)]
-	public LayoutState? CurrentState { get; set; }
+	public LayoutState CurrentState { get; set; }
 
 	public const int ID_CONST = -593368100;
 	public override int Id => ID_CONST;
